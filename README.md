d685334784f3893d99e1f2c11fe114d55d822a2d
iawflower <iawflower@gmail.com>
2013-10-08 15:46:07 -0700

    Updated comments and Readme

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 TextAdventure
 =============
 
-Text Adventure framework in Python.
+This python code implements a framework for a Text Adventure.
+
+Objects and Rooms:
+	The base classes in this framework are Objects, which represent things in the game you can interact with, and Rooms, which represent housings for these objects.
+	Individual objects are implemented as subclasses of the Object type, which allows them to inherit general properties, while still having object-unique behavior.
+	Rooms are also implemented as subclasses, allowing for room-unique behavior, including commands that are associated with specific rooms.
+	
+Command parsing:
+	Player commands are parsed by looking for keywords and ignoring everything else. This can create some powerful, and some counter-intuitive behavior.
+	For example, imagine you want to allow the play go through a door into a closet. Then, depending on which keywords you use, the following could all trigger the event:
+		"Enter closet"
+		"Enter the closet"
+		"Go through closet door"
+		"Go into the closet"
+		"Use closet door"
+		"Use closet"
+		"Do not go into the closet"
+		"Run away from closet door enterance" [sic]
+	This is based on the assumption that players are more likely to use positive commands than negative ones. If they do, they will find counter-intuitive behavior, which is
+	unfortunate, but a byproduct of this method.
+	
+Right now, objects can interact with each other by adding the names of each object to the other's dictionary or interactions, but I would like to make this easier, and more
+intuitive to code.
+
+For that matter, I'm also working on creating a better method of adding interactions to objects, so that adding new interactions will be more intuitive.
--- TextAdventure.py
+++ TextAdventure.py
@@ -1,33 +1,39 @@
+#
+#	The Player class is a collections of methods designed to represent the player's interactions. It includes more of the i/o information, as well
+#	as methods for interacting with the inventory, list of actions done, and location.
+#
+
 class Player:
 	def __init__(self):
-		self.actionsDone = []
+		self.actionsDone = []	# List of actions performed so far. Unaccessable by the player, this list just helps the game figure out what you've done so far.
+								# Note that this is a list of strings, so spelling matters!
 	
-	def hasItem(self,item):
+	def hasItem(self,item): # Checks the requested item name against the items in the inventory. Note that it uses the string type names associated with each object.
 		tempbool = False
 		for x in inventory.objects:
 			if x.name == item.name:
 				tempbool = True
 		return tempbool
 	
-	def addDone(self,done):
-		if (done != "null") and (done not in self.actionsDone):
+	def hasDone(self,action): # Checks if the player has done that particular action yet.
+		return (action in self.actionsDone)
+	
+	def addDone(self,done): # Adds a string to the list of actions done.
+		if (done not in self.actionsDone):
 			self.actionsDone.append(done)
 			
-	def delDone(self,done):
-		if (done != "null"):
+	def delDone(self,done): # Removes a given string from the list of actions done.
+		if (done in self.actionsDone):
 			self.actionsDone.remove(done)
 	
-	def printInventory(self):
+	def printInventory(self): # Prints out the inventory, using the item name strings. The player can access this list whenever they need to during the game.
 		for x in inventory.objects:
 			print (x.name)
 	
-	def printDone(self):
+	def printDone(self): # Prints the list of actions done. Used for debugging, not intended to be accessible by player.
 		print (self.actionsDone)
 	
-	def move(self,place):
-		if (place != "null"):
-			place.enter(self)
-	
+	# Location is a property of the Player class object. It provides access to the room that the player currently resides in.
 	def setLoc(self,place):
 		self._location = place
 	
@@ -36,23 +42,34 @@ class Player:
 	
 	location = property(getLoc,setLoc)
 	
+	#
+	#	Prompt is the most important function in this framework. It asks the player for an input string, and then uses that to determine
+	#	what action they are trying to perform. It works by looking for possibilities in this order:
+	#		1. Checks it the command in its entirety is one of a few "general commands." If so, it simply executes that command.
+	#		2. Checks if the command in its entirety is in the list of "override commands" for the room the player is in. If so, it simply executes that command.
+	#		3. Otherwise, it scans the command looking for the names of objects either in the player's inventory of in the room they are in.
+	#			a. If it finds an object, it checks if any keywords associated with that object are in the command. If so, it executes the command associated with that keyword.
+	#		4. If it hasn't yet done something, it responds "I don't understand."
+	#	Finally, it calls itself again, to ask the player for another input.
+	#
 	def prompt(self):
+		# Gets input.
 		command = input(">>  ")
-		self.interpret(command)
-		
-	def interpret(self,command):
-		command = command.lower()
+		command = command.lower() # makes it lower case to match names
 		here = self.location
+		# Checks if the command is one of the "general commands."
 		if command == "inventory":
 			self.printInventory()
 		elif command == "look around":
 			here.visited = False
-			here.enter(self)
+			here.enter()
 		elif command == "actions":
 			self.printDone()
-		elif command in here.actions:
-			here.performOverride(command)
+		# Checks if the command is one of the "override commands" for the room.
+		elif command in here.overrides:
+			here.overrides[command]()
 		else: 
+			# Looks for object names.
 			objectfound = 0
 			commandstring = command.partition(" ")
 			while (commandstring[0] != "") and objectfound == 0:
@@ -63,164 +80,211 @@ class Player:
 					if commandstring[0] == object.name:
 						objectfound = object
 				commandstring = commandstring[2].partition(" ")
+			# If it finds one, calls performAction, which will evaluate for commands.
 			if objectfound != 0:
 				self.performAction(here,objectfound,command)
+			# If no object found, and no commands matched above, returns "I don't understand."
 			else:
 				print ("I don't understand.")
+		# Reexecutes the prompt.
 		self.prompt()
 			
-		
+	#
+	#	
+	#
 	def performAction(self,location,object,command):
-		action = []
-		commandstring = command.partition(" ")
 		for x in object.interacts:
 			if x in command:
-				action = object.interacts.get(x)
-		if action != []:
-			if (action[0] != "null") and (not self.hasItem(action[0])):
-				print(action[1])
-			elif (action[2] != "null") and (action[2] not in player.actionsDone):
-				print(action[3])
-			else:
-				if (action[4] != "null"):
-					print(action[4])
-				if (action[5] != "null"):
-					action[5].move(inventory)
-				if (action[6] != "null"):
-					action[6].move(storage)
-				player.addDone(action[7])
-				player.move(action[8])
-		else:
-			print ("I don't understand.")
+				object.interacts[x]()
+				break
+
+
+#
+#	Objects are things you can directly interact with, and are designed to be called by name in a command.
+#	Each object keeps track of which room it is in, which includes the player's inventory.
+#	An object's most important feature is its list of interactions, which is stored as a dictionary.
+#	Each object in the game is initiated as a subclass of the object class, which gives it the initialization it needs.
+#				
+class object:						# When created, it requires two arguments: the room it should be created in, and the name it should be called by.
+	def __init__(self,room,text):	# Subclasses shouldn't override this unless absolutely necessary.
+		room.addObject(self)		# Uses the room init argument, adds itself to that room.
+		self.name = text			# Saves the name given.
+		self.here = room			# Saves currently location.
+		self.interacts = {}			# Dictionary of interactions.
 		
-class object:
-	def __init__(self,room,text):
-		room.addObject(self)
-		self.interacts = {}
-		self.name = text
-		self.here = room
-		
+		#
+		#	The dictionary of interactions works as a dispatch table. It stores function calls as the entries, and uses keywords as the entry titles.
+		#	Each object will have, inherently, the interactions listed below, but they need to have the functions defined on a per object basis, or
+		#	they will default to the boring versions given here.
+		#
+		self.addInteracts()
+	
+	#
+	#	In order to define new interactions, each object should redefine the addInteracts(self) function, adding new entries to the interacts dictionary.
+	#	For example: self.interacts["new action"] = self.doSomething
+	#	and then, later in the subclass definition, doSomething(self) must be defined.
+	#
+	
+	def addInteracts(self):
+		{}
+	
+	# Allows the object to be moved around. Moving to inventory gives the item to the player. There is also a room called storage where objects can be placed temporarily.
 	def move(self,room):
 		self.here.removeObject(self)
 		room.addObject(self)
-		
-	def addInteract(self,strings,action):
-		for string in strings:
-			self.interacts[string] = action
-		
-	def setDesc(self,info):
-		self._desc = info.capitalize()
-	
-	def readDesc(self):
-		return self._desc
-	
-	desc = property(readDesc, setDesc)
-	
+
+#
+#	Rooms are places you, and objects, can reside in. They keep track of which objects are in them, and also a dictionary of override commands that can
+#	be called in just that room. These override commands are defined the exact same way as object interactions, using function calls and dictionary entries.
+#	They also feature an "enter" method, which does a few things.
+#		It places the player into that room.
+#		First, it can perform any special behaviors defined in the enterSpecial(self) method for that object.
+#		Second, it prints a message associated with that room. If player is entering the room for the first time, it prints a detailed description of the room.
+#			otherwise, it prints a simple description.
+#
 class Room:
 	def __init__(self):
 		self.objects = []
-		self.actions = {}
-		self.condItem = {}
-		self.condDone = {}
+		self.overrides = {}
+		self.addOverrides()
 		self.visited = False
-
+	
 	def addObject(self,object):
 		self.objects.append(object)
 		
 	def removeObject(self,object):
 		self.objects.remove(object)
 	
-	def enter(self,player):
+	def printDesc(self):
+		print("\nGeneric room with no description")
+		
+	def printDet(self):
+		print("\nDetailed description of generic room")
+		
+	def enterSpecial(self):
+		{}
+	
+	def enter(self):
+		self.enterSpecial()
 		if (self.visited):
-			print (self.desc)
+			self.printDesc()
 		else:
-			print (self.det)
+			self.printDet()
 			self.visited = True
-		for x in self.condItem:
-			if x in player.inventory:
-				print (self.condItem.get(x))
-		for x in self.condDone:
-			if x in player.actionsDone:
-				print (self.condDone.get(x))
 		player.location = self
+	
+	def addOverrides(self):
+		{}
+
+
+player = Player()		#	Player class.
+inventory = Room()		#	Player's inventory.
+storage = Room()		#	Void storage for objects that are out of use.
+
+###########################
+#
+#	Rooms in the game
+#
+###########################
+class entrance(Room):
+	def printDesc(self):
+		print("You are in the enterance hall.")
 		
-	def performOverride(self,command):
-		action = self.actions.get(command)
-		if (action[0] != "null") and (not player.hasItem(action[0])):
-			print(action[1])
-		elif (action[2] != "null") and (action[2] not in player.actionsDone):
-			print(action[3])
-		else:
-			if (action[4] != "null"):
-				print(action[4])
-			if (action[5] != "null"):
-				action[5].move(inventory)
-			if (action[6] != "null"):
-				action[6].move(storage)
-			player.addDone(action[7])
-			player.move(action[8])
+	def printDet(self):
+		print("An uninteresting entrance hall. You wonder what's going on. There is a closet. There is a dark corner. What would you like to do?")
+		if ("sang" in player.actionsDone):
+			print ("You can hear the echoes of your song.")
 			
-	def addAction(self,triggers,action):
-		for trigger in triggers:
-			self.actions[trigger] = action
-	
-	def setDesc(self,info):
-		self._desc = info.capitalize()
-	
-	def readDesc(self):
-		return self._desc
-	
-	desc = property(readDesc, setDesc)
-	
-	def setDet(self,info):
-		self._det = info.capitalize()
-	
-	def readDet(self):
-		return self._det
-	
-	det = property(readDet, setDet)
-	
-	def addCondItem(self,item,text):
-		self.condItem[item] = text
+	def addOverrides(self):
+		self.overrides["sing"] = self.sing
 		
-	def addCondDone(self,done,text):
-		self.condDone[done] = text
+	def sing(self):
+		print("Sounded beautiful.")
+		player.addDone("sang")	
+entrance = entrance()
+
+class closet(Room):
+	def printDet(self):
+		print("It's a dark and musty closet. There are jackets everywhere.")
+		if	(batteries in self.objects):
+			print ("In one of the jacket pockets, there are batteries.")
+	
+	def printDesc(self):
+		print("\nYou are in the closet.")
 		
-player = Player()
-inventory = Room()
-storage = Room()
+	def addOverrides(self):
+		self.overrides["leave"] = self.leave
+		self.overrides["exit"] = self.leave
+		self.overrides["go"] = self.leave
+		self.overrides["back"] = self.leave
+		self.overrides["go back"] = self.leave
+		
+	def leave(self):
+		entrance.enter()
+closet = closet()
 
-vocalcords = object(inventory,"vocalcords")
-
-entrance = Room()
-entrance.setDet("An uninteresting entrance hall. You wonder what's going on. There is a closet. There is a dark corner. What would you like to do?")
-entrance.setDesc("You are in the enterance hall.")
-entrance.addCondDone("sang","You can hear the echoes of your song.")
-
-closet = Room()
-closet.setDet("It's a dark and musty closet. There are jackets everywhere.")
-closet.setDesc("You are in the closet.")
-
-flashlight = object(entrance,"flashlight")
-flashlight.setDesc ("It's a normal flashlight.")
-flashlight.addInteract(["take","pick up"],["null","null","null","null","You picked up a flashlight. Missing it's batteries, though.",flashlight,"null","null","null"])
-
-batteries = object(closet,"batteries")
-batteries.setDesc("D-type batteries. Perfect for a flashlight.")
-
-flashlight.addInteract(["batteries"],[batteries,"You don't have any batteries.","null","null","You put the batteries in the flashlight.","null",batteries,"flashlight charged","null"])
 ###########################
 #
 #	Objects in Entrance
 #
 ###########################
-entrance.addAction(["sing"],[vocalcords,"Hah, with what vocal cords?","null","You're not experienced enough yet.","Sounded beautiful.","null","null","sang","null"])
-lamp = object(entrance,"lamp")
-lamp.addInteract(["look","examine","inspect"],["null","null","null","null","It's a non-descript lamp. Not very interesting.","null","null","inspected lamp","null"])
-corner = object(entrance,"corner")
-corner.addInteract(["illuminate","light"],[flashlight,"You don't have a flashlight.","flashlight charged","You're flashlight is dead","You illuminate the corner with your flashlight. Cowering there is a scared cat.","null","null","illuminated corner","null"])
-closetdoor = object(entrance,"closet")
-closetdoor.addInteract(["enter","use"],["null","null","null","null","null","null","null","null",closet])
+class flashlight(object):
+	def addInteracts(self):
+		self.interacts["inspect"] = self.inspect
+		self.interacts["take"] = self.take
+		self.interacts["pick up"] = self.take
+		self.interacts["batteries"] = self.batteries
+		
+	def take(self):
+		print("You picked up a flashlight. Missing its batteries, though.")
+		self.move(inventory)
+		
+	def inspect(self):
+		if("flashlight charged" in player.actionsDone):
+			print("It's a normal flashlight.")
+		else:
+			print("It's a normal flashlight. Missing its batteries, though.")
+			
+	def batteries(self):
+		if player.hasItem(batteries):
+			batteries.move(storage)
+			player.addDone("flashlight charged")
+		else:
+			print ("I don't understand.")
+flashlight = flashlight(entrance,"flashlight")
+
+
+class lamp(object):
+	def inspect(self):
+		print("It's a non-descript lamp. It dimly illuminates the room, but plenty of shadows remain.")
+		player.addDone("inspected lamp")
+lamp = lamp(entrance,"lamp")
+
+
+class corner(object):
+	def addInteracts(self):
+		self.interacts["illuminate"] = self.light
+		self.interacts["light"] = self.light
+		
+	def light(self):
+		if (not player.hasItem(flashlight)):
+			print("You don't have a flashlight")
+		elif (not player.hasDone("flashlight charged")):
+			print("Your flashlight is dead.")
+		else:
+			print("You illuminate the corner with your flashlight. Cowering there is a scared cat. It hisses.")
+			player.addDone("illuminated corner")
+corner = corner(entrance,"corner")
+
+
+class closetdoor(object):
+	def addInteracts(self):
+		self.interacts["enter"] = self.gothrough
+		self.interacts["use"] = self.gothrough
+		
+	def gothrough(self):
+		closet.enter()
+closetdoor = closetdoor(entrance,"closet")
 
 
 ###########################
@@ -228,9 +292,30 @@ closetdoor.addInteract(["enter","use"],["null","null","null","null","null","null
 #	Objects in Closet
 #
 ###########################
-closet.addAction(["exit","leave","return"],["null","null","null","null","null","null","null","null",entrance])
-batteries = object(closet,"batteries")
-batteries.addInteract("take",["null","null","null","null","You've acquired batteries.",batteries,"null","null","null"])
+class batteries(object):
+	def addInteracts(self):
+		self.interacts["inspect"] = self.inspect
+		self.interacts["take"] = self.take
+		self.interacts["pick up"] = self.take
+		self.interacts["flashlight"] = self.flashlight
+		
+	def take(self):
+		print("You've acquired batteries.")
+		self.move(inventory)
+		
+	def inspect(self):
+		print("D-type batteries. Perfect for a flashlight.")
+		
+	def flashlight(self):
+		if player.hasItem(flashlight):
+			self.move(storage)
+			player.addDone("flashlight charged")
+		else:
+			print ("I don't understand.")
+			
+batteries = batteries(closet,"batteries")
 
-entrance.enter(player)
-player.prompt()
+
+
+entrance.enter()	#	Places the player in the entrance.
+player.prompt()		#	Initiates the first prompt command. The game is afoot!
